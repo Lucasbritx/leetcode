2e57ed4ea73eab0878f00a8d1fbeec10860da0fe
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-06-14 23:22:14 -0300

    217 contains duplicated

---
+++ 217-contains-duplicate.scala
@@ -0,0 +1,13 @@
+import scala.collection.mutable.HashSet
+
+object Solution {
+    def containsDuplicate(nums: Array[Int]): Boolean = {
+        var hasDuplicated = false
+        val seen = HashSet[Int]()
+        for (num <- nums) {
+            if (seen.contains(num)) hasDuplicated = true
+            seen += num
+        }
+        hasDuplicated
+    }
+}
