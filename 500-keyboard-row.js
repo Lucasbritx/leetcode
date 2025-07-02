7e4ed9f416143e82ed9a0a8f39df3f445903d258
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-07-01 22:48:15 -0300

    500 keyboard row exercise

---
+++ 500-keyboard-row.js
@@ -0,0 +1,26 @@
+/* https://leetcode.com/problems/keyboard-row/ */
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+const firstRow = "qwertyuiop".split("");
+const secondRow = "asdfghjkl".split("");
+const thirdRow = "zxcvbnm".split("");
+var findWords = function (words) {
+  const result = [];
+  words.map((word) => {
+    let haveOnlyFirstRow = true;
+    let haveOnlySecondRow = true;
+    let haveOnlyThirdRow = true;
+    word.split("").map((letter) => {
+      if (!firstRow.includes(letter.toLowerCase())) haveOnlyFirstRow = false;
+      if (!secondRow.includes(letter.toLowerCase())) haveOnlySecondRow = false;
+      if (!thirdRow.includes(letter.toLowerCase())) haveOnlyThirdRow = false;
+    });
+    console.log({ haveOnlyFirstRow, haveOnlySecondRow, haveOnlyThirdRow });
+    if (haveOnlyFirstRow || haveOnlySecondRow || haveOnlyThirdRow) {
+      result.push(word);
+    }
+  });
+  return result;
+};
