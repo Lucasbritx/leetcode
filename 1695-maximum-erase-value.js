54ca15abdd4c512a02cf521920acb95e4ded235f
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-07-22 12:00:07 -0300

    1695 maximum erase value

--- 1695-maximum-erase-value.js
+++ 1695-maximum-erase-value.js
@@ -2,12 +2,10 @@
  * @param {number[]} nums
  * @return {number}
  */
+/* https://leetcode.com/problems/maximum-erasure-value/?envType=daily-question&envId=2025-07-22 */
+/* WRONG need to fix, instead unique values create subarrays to see highest value */
 const maximumUniqueSubarray = function (nums) {
-  nums.sort((a, b) => a - b);
-  const uniqueValues = [...new Set(nums)];
-  let counter = 0;
-  uniqueValues.map((value) => {
-    counter += value;
-  });
-  return counter;
+    const uniqueValues = [...new Set(nums)];
+    const counter = uniqueValues.reduce((a, b) => a + b, 0);
+    return counter;
 };
