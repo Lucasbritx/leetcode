e1598b70a935c3ff063710b4828cdd2a3bb8cabb
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-01-08 17:34:37 -0300

    neetcode 150 contains duplicate

---
+++ neetcode-150-contains-duplicate.js
@@ -0,0 +1,15 @@
+/* https://neetcode.io/problems/duplicate-integer/question */
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums) {
+    const seen = new Set();
+    for (const item of nums) {
+      if (seen.has(item)) return true;
+      seen.add(item);
+    }
+    return false;
+  }
+}
