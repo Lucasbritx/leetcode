c4e5c3b64d4169e7fa601e459c123f9e3fdb4970
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-01-15 23:53:36 -0300

    neetcode is palindrome with two pointers

--- neetcode-is-palindrome.js
+++ neetcode-is-palindrome.js
@@ -1,13 +1,27 @@
 /* https://neetcode.io/problems/is-palindrome/question?list=blind75 */
 class Solution {
-    /**
-     * @param {string} s
-     * @return {boolean}
-     */
-    isPalindrome(s) {
-        const string = s.toLowerCase()
-            .replace(/[^a-z0-9]/g, '');
-
-        return string === string.split('').reverse().join('');
+  isInvalidChar(char) {
+    return !/[a-z0-9]/i.test(char);
+  }
+  /**
+   * @param {string} s
+   * @return {boolean}
+   */
+  isPalindrome(s) {
+    for (let i = 0, j = s.length - 1; i < j; ) {
+      const leftValue = s[i];
+      const rightValue = s[j];
+      if (this.isInvalidChar(leftValue)) {
+        i++;
+      } else if (this.isInvalidChar(rightValue)) {
+        j--;
+      } else {
+        if (leftValue.toLowerCase() !== rightValue.toLowerCase()) return false;
+        i++;
+        j--;
+      }
     }
+
+    return true;
+  }
 }
